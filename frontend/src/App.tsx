3835b56cba37d080f1c15fd3033fa06cf3d72f08
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-08-08 01:52:17 +0530

    second commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <input type="text" placeholder='Message...' />
+      <button>Send</button>
+    </>
+  )
+}
+
+export default App
